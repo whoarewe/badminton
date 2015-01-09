7134078e94cb469286e63f2961bf7adc3ba2ea3b
Frank <tonghuazhu@gmail.com>
2015-01-10 01:44:59 +0800

    build scalatra project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-badminton
-=========
+# badminton #
+
+## Build & Run ##
+
+```sh
+$ cd badminton
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/github/zinima/badminton/BadmintonServlet.scala
@@ -0,0 +1,17 @@
+package com.github.zinima.badminton
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class BadmintonServlet extends BadmintonStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
